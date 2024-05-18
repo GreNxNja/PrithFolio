a787f81a8d899fd9d8658ab461be4de21306582c
Basilisk <prith.tiu13@gmail.com>
2024-05-18 12:09:42 +0530

    add globe gradient backgrnd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { navItems } from "@/data";
-
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
@@ -9,7 +9,6 @@ import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
